b300c74854a35818bd628094a2a3e56df12400fa
sunrain990 <409747494@qq.com>
2018-06-24 19:13:42 +0800

    03router f

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {ProductComponent} from './product/product.component';
+import {Code404Component} from './code404/code404.component';
+import {ProductDescComponent} from './product-desc/product-desc.component';
+import {SellerInfoComponent} from './seller-info/seller-info.component';
+import {ChatComponent} from './chat/chat.component';
+import {LoginGuid} from './guard/login.guard';
+import {UnsavedGuard} from './guard/unsaved.guard';
+import {ProductResolve} from './guard/product.resolve';
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'chat', component: ChatComponent, outlet: 'aux' },
+  { path: 'home', component: HomeComponent },
+  // { path: 'product', component: ProductComponent },
+  { path: 'product/:id', component: ProductComponent , children: [
+      { path: '', component: ProductDescComponent },
+      { path: 'seller/:id', component: SellerInfoComponent }
+
+  ],  resolve: {
+    product: ProductResolve
+  }},
+  { path: '**', component: Code404Component }
 ];
+// canActivate: [ LoginGuid ],
+// canDeactivate: [UnsavedGuard]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [LoginGuid, UnsavedGuard, ProductResolve]
 })
 export class AppRoutingModule { }
