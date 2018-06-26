56d857499f92c01848cdc14f9187711bd307e887
sunrain990 <409747494@qq.com>
2018-06-27 01:33:52 +0800

    06hook f

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -12,6 +12,11 @@ export class ProductDetailComponent implements OnInit {
 
   comments: Comment[];
 
+  newRating: number = 5;
+  newComment: string = '';
+
+  isCommentHidden = true;
+
   constructor(private routeInfo: ActivatedRoute,
               private productService: ProductService) { }
 
@@ -19,6 +24,17 @@ export class ProductDetailComponent implements OnInit {
     const productId: number = this.routeInfo.snapshot.params['productId'];
     this.product = this.productService.getProduct(productId);
     this.comments = this.productService.getCommentsForProductId(productId);
+  }
+
+  addComment() {
+    let comment = new Comment(0, this.product.id, new Date().toISOString(), 'someone', this.newRating, this.newComment);
+    this.comments.unshift(comment);
+    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.product.rating = sum / this.comments.length;
+
+    this.newComment = null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
   }
 
 }
